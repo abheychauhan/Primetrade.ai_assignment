6ad4ac29876a584dfe5c93197d5f55218485b999
Abhey Singh <abheys172@gmail.com>
2025-11-07 21:22:14 +0530

    f

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -16,8 +16,8 @@ export const registerUser = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false, 
-      sameSite: "lax",
+      secure:"production", 
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -44,8 +44,8 @@ export const loginUser = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "lax",
+      secure: "production",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
